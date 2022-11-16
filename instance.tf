93aa3f820bab25667c482065a0616a801a81809e
Sathya654 <115213543+Sathya654@users.noreply.github.com>
2022-11-16 19:01:17 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "sa-east-1"
+  region = "us-east-1"
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_SECRET_KEY 
 }
@@ -8,6 +8,6 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Hopp1"
   }
 }
