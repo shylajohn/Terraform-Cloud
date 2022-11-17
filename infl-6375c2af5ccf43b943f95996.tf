d09d1ae87ae0a3b533ed51b9837c9c01456ec86d
Sathya654 <115213543+Sathya654@users.noreply.github.com>
2022-11-17 10:42:16 +0530

    Codification: aws_cloudwatch_log_group

---
+++ infl-6375c2af5ccf43b943f95996.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudwatch_log_group" "awslambdahelm-provider-vpc-connector-0a3f07aabe6dda20455a1cd03161066a-05f" {
+  name = "/aws/lambda/helm-provider-vpc-connector-0a3f07aabe6dda20455a1cd03161066a"
+}
+
+
+
+
+resource "aws_cloudwatch_log_group" "awslambdaawsqs-kubernetes-resource-proxy-TanzuCluster-83a" {
+  name = "/aws/lambda/awsqs-kubernetes-resource-proxy-TanzuCluster"
+}
+
+
+
+
+resource "aws_cloudwatch_log_group" "awseksTanzuClustercluster-a8a" {
+  name = "/aws/eks/TanzuCluster/cluster"
+}
+
