8713bc62e1c01b6da6b79ec7870c9ccbfa877960
shylajohn <70839821+shylajohn@users.noreply.github.com>
2024-01-30 12:37:38 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,13 +1,4 @@
-provider "aws" {
-  region = "sa-east-1"
-  access_key=var.AWS_ACCESS_KEY
-  secret_key=var.AWS_SECRET_KEY 
-}
-resource "aws_instance" "web" {
-  ami           = "ami-02dc8ad50da58fffd"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+resource "azurerm_resource_group" "example" {
+  name     = var.rg-name
+  location = var.location
 }
